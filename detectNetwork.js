895dfcb0445a530277f1c601418e7f39bcbd9613
Chris Hesterman <spoiler14@comcast.net>
2020-01-29 15:44:58 -0800

    Make minor formatting changes.

--- detectNetwork.js
+++ detectNetwork.js
@@ -15,67 +15,60 @@ var detectNetwork = function(cardNumber) {
   const preSix = parseInt(cardNumber.slice(0, 6));
   const length = cardNumber.length;
 
-  const dinerClub = (preTwo === 38 || preTwo === 39)
-                    && length === 14;
+  const dinerClub = (preTwo === 38 || preTwo === 39) && length === 14;
 
-  const amex = (preTwo === 34 || preTwo === 37)
-                && length === 15;
+  const amex = (preTwo === 34 || preTwo === 37) && length === 15;
 
-  const visa = preOne === 4
-              &&
-              (length === 13
-              || length === 16
-              || length === 19);
+  const visa =
+    preOne === 4 && (length === 13 || length === 16 || length === 19);
 
-  const masterCard = (preTwo >= 51 && preTwo <= 55)
-                      && length === 16;
+  const masterCard = preTwo >= 51 && preTwo <= 55 && length === 16;
 
-  const discover = (preFour === 6011
-                    || preTwo === 65
-                    || (preThree >= 644 && preThree <= 649))
-                    &&
-                    (length === 16 || length === 19);
+  const discover =
+    (preFour === 6011 ||
+      preTwo === 65 ||
+      (preThree >= 644 && preThree <= 649)) &&
+    (length === 16 || length === 19);
 
-  const maestro = (preFour === 5018
-                  || preFour === 5020
-                  || preFour === 5038
-                  || preFour === 6304)
-                  &&
-                  (length >= 12 && length <= 19);
+  const maestro =
+    (preFour === 5018 ||
+      preFour === 5020 ||
+      preFour === 5038 ||
+      preFour === 6304) &&
+    length >= 12 &&
+    length <= 19;
 
-  const chinaUnion = (length >= 16 && length <= 19)
-                      && ((preSix >= 622126 && preSix <= 622925)
-                      || (preThree >= 624 && preThree <= 626)
-                      || (preFour >= 6282 && preFour <= 6288))
+  const chinaUnion =
+    length >= 16 &&
+    length <= 19 &&
+    ((preSix >= 622126 && preSix <= 622925) ||
+      (preThree >= 624 && preThree <= 626) ||
+      (preFour >= 6282 && preFour <= 6288));
 
-  const switchCard = (preFour === 4903
-                    || preFour === 4905
-                    || preFour === 4911
-                    || preFour === 4936
-                    || preFour === 6333
-                    || preFour === 6759
-                    || preSix === 564182
-                    || preSix === 633110)
-                    &&
-                    (length === 16
-                    ||length === 18
-                    ||length === 19)
+  const switchCard =
+    (preFour === 4903 ||
+      preFour === 4905 ||
+      preFour === 4911 ||
+      preFour === 4936 ||
+      preFour === 6333 ||
+      preFour === 6759 ||
+      preSix === 564182 ||
+      preSix === 633110) &&
+    (length === 16 || length === 18 || length === 19);
 
   let cardName;
 
-  if (dinerClub) cardName = 'Diner\'s Club';
-  if (amex) cardName = 'American Express';
-  if (visa === true && switchCard !== true) cardName = 'Visa';
-  if (masterCard) cardName = 'MasterCard';
-  if (discover) cardName = 'Discover';
-  if (maestro) cardName = 'Maestro';
-  if (chinaUnion) cardName = 'China UnionPay';
-  if (switchCard) cardName = 'Switch';
+  if (dinerClub) {cardName = "Diner's Club"};
+  if (amex) cardName = {'American Express'};
+  if (visa === true && switchCard !== true) {cardName = 'Visa'};
+  if (masterCard) {cardName = 'MasterCard'};
+  if (discover) {cardName = 'Discover'};
+  if (maestro) {cardName = 'Maestro'};
+  if (chinaUnion) {cardName = 'China UnionPay'};
+  if (switchCard) {cardName = 'Switch'};
 
   return cardName;
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
-
